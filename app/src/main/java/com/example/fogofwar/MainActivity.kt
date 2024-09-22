eaf2ce04660a8a8af5d8fac3ef44abe3abd806f5
th3caucasian <alan-erkenov@mail.ru>
2024-09-22 21:12:17 +0300

    Коммит перед изменениями

--- app/src/main/java/com/example/fogofwar/MainActivity.kt
+++ app/src/main/java/com/example/fogofwar/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.fogofwar
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Rect
-import android.graphics.drawable.BitmapDrawable
-import android.location.GpsStatus
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.LocaleListCompat
 import com.example.fogofwar.databinding.ActivityMainBinding
-import com.google.android.gms.maps.model.*
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.events.MapListener
@@ -24,13 +22,12 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
-class MainActivity : AppCompatActivity(), MapListener, GpsStatus.Listener {
-    lateinit var mMap: MapView
-    lateinit var controller: IMapController
-    lateinit var mMyLocationOverlay: MyLocationNewOverlay
-    lateinit var mButton: Button
-    lateinit var currentIcon: Bitmap
-    lateinit var scaledIcon: Bitmap
+class MainActivity : AppCompatActivity(), MapListener {
+    private lateinit var mMap: MapView
+    private lateinit var controller: IMapController
+    private lateinit var mMyLocationOverlay: MyLocationNewOverlay
+    private lateinit var currentIcon: Bitmap
+    private lateinit var scaledIcon: Bitmap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -46,11 +43,19 @@ class MainActivity : AppCompatActivity(), MapListener, GpsStatus.Listener {
         mMap.getLocalVisibleRect(Rect())
 
         mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mMap)
+//        {
+//            override fun onLocationChanged(location: Location?, source: GpsMyLocationProvider?) {
+//                super.onLocationChanged(location, source)
+//            }
+//        }
+
         controller = mMap.controller
 
+
+        val gpsProvider = GpsMyLocationProvider(this)
         mMyLocationOverlay.enableMyLocation()
         mMyLocationOverlay.enableFollowLocation()
-        mMyLocationOverlay.isDrawAccuracyEnabled()       // ???
+        mMyLocationOverlay.isDrawAccuracyEnabled        // ???
         mMyLocationOverlay.setPersonAnchor(0.5F, 0.5F)
         mMyLocationOverlay.runOnFirstFix {
             runOnUiThread {
@@ -58,13 +63,9 @@ class MainActivity : AppCompatActivity(), MapListener, GpsStatus.Listener {
                 controller.animateTo(mMyLocationOverlay.myLocation)
             }
         }
-
-        currentIcon = BitmapFactory.decodeResource(resources, R.drawable.location_arrow_2)                // Если использовать эту переменную - иконка не отображается (видимо дело в размере)
+        currentIcon = BitmapFactory.decodeResource(resources, R.drawable.location_arrow_2)        // Если использовать эту переменную - иконка не отображается (видимо дело в размере)
         scaledIcon = Bitmap.createScaledBitmap(currentIcon, 50, 50, true)  // Если использовать эту - то всё отлично работает
-        if (currentIcon == null)
-            Log.e("TAG", "ICON FAIL")
-        else
-            mMyLocationOverlay.setPersonIcon(scaledIcon)
+        mMyLocationOverlay.setPersonIcon(scaledIcon)
 
 
         controller.setZoom(18.0)
@@ -75,19 +76,16 @@ class MainActivity : AppCompatActivity(), MapListener, GpsStatus.Listener {
     }
 
     override fun onScroll(event: ScrollEvent?): Boolean {
-        Log.e("TAG", "onCreate:la ${event?.source?.getMapCenter()?.latitude}")
-        Log.e("TAG", "onCreate:lo ${event?.source?.getMapCenter()?.longitude}")
+        Log.e("TAG", "onCreate:la ${event?.source?.mapCenter?.latitude}")
+        Log.e("TAG", "onCreate:lo ${event?.source?.mapCenter?.longitude}")
         return true
     }
 
     override fun onZoom(event:ZoomEvent?): Boolean {
         Log.e("TAG", "onZoom zoom level: ${event?.zoomLevel} source ${event?.source}")
-        return false;
+        return false
     }
 
-    override fun onGpsStatusChanged(event: Int) {
-        TODO("Not yet implemented")
-    }
 
     fun toMyLocationButton(view: View?) {
         runOnUiThread {
