e75049187b2a345d25a2661415a4b894580aa54b
th3caucasian <alan-erkenov@mail.ru>
2024-10-26 16:54:40 +0300

    Отслеживание локации с помощью FusedLocationClientProvider вместо стандартного osmdroid

--- app/src/main/java/com/example/fogofwar/MainActivity.kt
+++ app/src/main/java/com/example/fogofwar/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fogofwar
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -9,11 +11,14 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.graphics.Rect
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.fogofwar.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.events.MapListener
@@ -33,9 +38,13 @@ class MainActivity : AppCompatActivity(), MapListener, SensorEventListener {
     private lateinit var currentIcon: Bitmap
     private lateinit var scaledIcon: Bitmap
     private lateinit var sensorManager: SensorManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
     private var azimuth = 0F
     private var prevAzimuth = 0F
     private var kFilter = KalmanFilter()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -47,29 +56,32 @@ class MainActivity : AppCompatActivity(), MapListener, SensorEventListener {
 
         mMap = binding.osmmap
         mMap.setTileSource(TileSourceFactory.MAPNIK)
-        mMap.mapCenter          // ???
         mMap.setMultiTouchControls(true)
         mMap.getLocalVisibleRect(Rect())
         controller = mMap.controller
         controller.setZoom(18.0)
 
-        mUpperOverlay = UpperOverlay()
-        mMap.overlays.add(mUpperOverlay)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateDistanceMeters(1f)
+            .setMinUpdateIntervalMillis(1000)
+            .build()
 
         mMyLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mMap)
         mMyLocationOverlay.enableMyLocation()
         mMyLocationOverlay.enableFollowLocation()
-        mMyLocationOverlay.isDrawAccuracyEnabled        // ???
+        mMyLocationOverlay.isDrawAccuracyEnabled = true
         mMyLocationOverlay.setPersonAnchor(0.5F, 0.5F)
-        mMyLocationOverlay.runOnFirstFix {
-            runOnUiThread {
-                val location = mMyLocationOverlay.myLocation
-                val geoPoint = GeoPoint(location.latitude, location.longitude)
-                controller.setCenter(location)
-                controller.animateTo(location)
-                mUpperOverlay.addClearedTile(geoPoint)
-            }
-        }
+//        mMyLocationOverlay.runOnFirstFix {
+//            runOnUiThread {
+//                val location = mMyLocationOverlay.myLocation
+//                val geoPoint = GeoPoint(location.latitude, location.longitude)
+//                controller.setCenter(location)
+//                controller.animateTo(location)
+//                mUpperOverlay.addClearedTile(geoPoint)
+//            }
+//        }
         currentIcon = BitmapFactory.decodeResource(resources, R.drawable.location_arrow_2)        // Если использовать эту переменную - иконка не отображается (видимо дело в размере)
         scaledIcon = Bitmap.createScaledBitmap(currentIcon, 100, 100, true)  // Если использовать эту - то всё отлично работает
         mMyLocationOverlay.setPersonIcon(scaledIcon)
@@ -84,10 +96,44 @@ class MainActivity : AppCompatActivity(), MapListener, SensorEventListener {
         sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI)
 
 
+        mUpperOverlay = UpperOverlay()
+        mMap.overlays.add(mUpperOverlay)
         mMap.overlays.add(mMyLocationOverlay)
-        mMap.addMapListener(this)
+        startLocationUpdates() //mMap.addMapListener(this)
 
     }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                for (location in locationResult.locations)
+                    updateMapLocation(location)
+            }
+        }, null)
+    }
+
+    private fun updateMapLocation(location: Location) {
+        val geoPoint = GeoPoint(location.latitude, location.longitude)
+        controller.setCenter(geoPoint)
+        controller.animateTo(geoPoint)
+        mUpperOverlay.addClearedTile(geoPoint)
+        Log.d("LOCATION_UPDATE", "Location: ${geoPoint.latitude}, ${geoPoint.longitude}")
+    }
+
 
     override fun onScroll(event: ScrollEvent?): Boolean {
         Log.e("TAG", "onCreate:la ${event?.source?.mapCenter?.latitude}")
@@ -109,20 +155,14 @@ class MainActivity : AppCompatActivity(), MapListener, SensorEventListener {
     }
 
     fun updateLocationButton(view: View?) {
-        runOnUiThread {
-            val location = mMyLocationOverlay.myLocation
-            val geoPoint = GeoPoint(location.latitude, location.longitude)
-            controller.setCenter(location)
-            controller.animateTo(location)
-            mUpperOverlay.addClearedTile(geoPoint)
-        }
+        // asddsad
     }
 
     private var gravity = FloatArray(3)
     private var geomagnetic = FloatArray(3)
     private var rotationMatrix = FloatArray(9)
     private var orientation = FloatArray(3)
-    private val smoothing = 15      // сглаживание значений азимута (чем больше - тем сильнее сглаживание)
+    private val smoothing = 15     // сглаживание значений азимута (чем больше - тем сильнее сглаживание)
     private var azimuths = mutableListOf<Float>()   // массив значений азимута
     private var avgAzimuth = 0F     // итоговое сглаженное значение азимута
 
@@ -133,7 +173,7 @@ class MainActivity : AppCompatActivity(), MapListener, SensorEventListener {
             azimuths.removeAt(0)
         avgAzimuth = azimuths.sum() / azimuths.size
         val rotMatrix = Matrix()
-        rotMatrix.postRotate(inAzimuth, scaledIcon.width / 2F, scaledIcon.height / 2F)
+        rotMatrix.postRotate(avgAzimuth, scaledIcon.width / 2F, scaledIcon.height / 2F)
         Log.e("AZIMUTH", "Azimuth: ${avgAzimuth}")
         val rotatedBitmap = Bitmap.createBitmap(scaledIcon,0,0,scaledIcon.width, scaledIcon.height, rotMatrix,true)
         mMyLocationOverlay.setPersonIcon(rotatedBitmap)
